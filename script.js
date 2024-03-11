cc5c0e559f56d5d3b2db9c0ffcc4c46968d654b8
kevin200k <mikimiki2194@gmail.com>
2024-03-11 18:41:27 +0100

    Function to perform different operation with the declared variables

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ let firstNumber;
 let secondNumber;
 let operand;
 
+function operate(firstNumber, operand, secondNumber){
+    switch (operand) {
+        case "+":
+            add(firstNumber, secondNumber)
+            break;
+        case "-":
+            subtract(firstNumber, secondNumber)
+            break;
+        case "/":
+            divide(firstNumber, secondNumber)
+            break;
+        case "*":
+            multiply(firstNumber, secondNumber)
+            break;
+        default:
+            break;
+    }
+}
+
