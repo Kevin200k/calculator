8189421facfa16c0a62abb041c341953167f9ab7
kevin200k <mikimiki2194@gmail.com>
2024-03-12 08:38:39 +0100

    Perform calculations with operate function

--- script.js
+++ script.js
@@ -15,26 +15,6 @@ let firstNumber;
 let secondNumber;
 let operand;
 
-//Function to perform the four operations on two numbers
-function operate(firstNumber, operand, secondNumber){
-    switch (operand) {
-        case "+":
-            add(firstNumber, secondNumber)
-            break;
-        case "-":
-            subtract(firstNumber, secondNumber)
-            break;
-        case "/":
-            divide(firstNumber, secondNumber)
-            break;
-        case "*":
-            multiply(firstNumber, secondNumber)
-            break;
-        default:
-            break;
-    }
-}
-
 //Declared variables for the the buttons
 let numberValues = document.querySelectorAll(".numbers");
 let operators = document.querySelectorAll(".operator");
@@ -50,13 +30,47 @@ numberValues.forEach(number => {
     number.addEventListener("click", function(){
         display.push(number.innerText)
         demo.innerText = display.join("")
-        console.log(display)       
     })
 })
 operators.forEach(operator =>{
     operator.addEventListener("click", function(){
+        operand = operator.innerText;
         display.push(operator.innerText);
         demo.innerText = display.join("")
-        console.log(display);
     })
 })
+
+//Event listeners for other buttons
+equals.addEventListener("click", operate);
+
+//Function to perform the four operations on two numbers
+function operate(firstNumber, secondNumber){
+    let p = display.indexOf(operand)
+    let v = display.length
+
+    firstNumber = display.slice(0, p).join("");
+    // console.log(firstNumber)
+    firstNumber = parseFloat(firstNumber)
+
+    secondNumber = display.slice(p + 1, v).join("");
+    // console.log(secondNumber)
+    secondNumber = parseFloat(secondNumber)
+   
+    switch (operand) {
+        case "+":
+            demo.innerText = add(firstNumber, secondNumber)
+            break;
+        case "-":
+            demo.innerText = subtract(firstNumber, secondNumber)
+            break;
+        case "/":
+            demo.innerText = divide(firstNumber, secondNumber)
+            break;
+        case "*":
+            demo.innerText = multiply(firstNumber, secondNumber)
+            break;
+        default:
+            break;
+    }
+}
+
