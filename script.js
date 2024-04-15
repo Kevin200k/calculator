83941cc7986fc606540651b35ec37735469895f5
kevin200k <mikimiki2194@gmail.com>
2024-04-15 21:13:40 +0100

    Added the operate function which allows performed operations between two numbers.

--- script.js
+++ script.js
@@ -1,17 +1,72 @@
+//DOM Elements
+let numbers = document.querySelectorAll(".numbers");
+let operators = document.querySelectorAll(".operator");
+let equalButton = document.querySelector("#equals")
+
 // Calculator variables
 const Calculator = {
     firstNumber: [],
     secondNumber: [],
+    currentOperator: "",
     display: []
 }
 
 // Function for mathematical operations
-const numericalOperations = (function(){
+const solve = (function(){
     add = (a, b) => a + b;
     subtract = (a, b) => a - b;
     multiply = (a, b) => a * b;
     divide = (a, b) => a / b;
     return { add, subtract, multiply, divide }
  }())
- 
+
  // Operate function
+ let operate = () => {
+    let index = Calculator.display.indexOf(Calculator.currentOperator);
+    Calculator.firstNumber = Calculator.display.slice(0, index)
+    Calculator.secondNumber = Calculator.display.slice(index + 1, Calculator.display.length)
+    
+    //Calculator variables
+    let x1 = parseFloat(Calculator.firstNumber.join(""));
+    let x2 = parseFloat(Calculator.secondNumber.join(""));
+    
+    // Switch statement to determine which operator to use
+    switch(Calculator.currentOperator){
+        case "+":
+            demo.textContent = solve.add(x1, x2);
+            break;
+        case "-":
+            demo.textContent = solve.subtract(x1, x2);
+            break;
+        case "*":
+            demo.textContent = solve.multiply(x1, x2);
+            break;
+        case "/":
+            demo.textContent = solve.divide(x1, x2);
+            break;
+    }
+    // Resets the display array and current operator
+    Calculator.display = [];
+    Calculator.display.push(demo.textContent)
+    Calculator.currentOperator = "";
+ }
+ 
+ //Event listeners to handle all number values
+ numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        Calculator.display.push(number.textContent)
+        demo.textContent = Calculator.display.join("")
+    })
+ })
+
+ //Event listeners to hanlde all operator values
+ operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+        Calculator.currentOperator = operator.textContent
+        Calculator.display.push(Calculator.currentOperator)
+        demo.textContent = Calculator.display.join("")
+    })
+ })
+
+ // Event listener for the equal sign
+equalButton.addEventListener("click", operate)
